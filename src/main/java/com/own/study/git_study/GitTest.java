7a2e168d4edd98c06a4a8b382f2ff16077791ba6
zhi zhao <zhi.zhao@taotu-partner.com>
2020-08-07 17:02:14 +0800

    update002

--- src/main/java/com/own/study/git_study/GitTest.java
+++ src/main/java/com/own/study/git_study/GitTest.java
@@ -5,7 +5,11 @@ package com.own.study.git_study;
  * @date 8/7/2020 4:39 PM
  */
 public class GitTest {
-    public void update001(){
+    public void update001() {
         System.out.println("update001");
+    }
+
+    public void update002(){
+        System.out.println("update002");
     }
 }
